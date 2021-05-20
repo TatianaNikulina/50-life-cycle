f23f2fab55fcf3683b10c8e14e435b583bd88529
Ignaty Kashnitsky <noir.ik@gmail.com>
2021-05-20 08:23:58 +0200

    Add drinks example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
-import App from './App';
+import Drinks from "./components/Drinks";
 
 ReactDOM.render(
-    <App />,
+    <Drinks />,
   document.getElementById('root')
 );
 
